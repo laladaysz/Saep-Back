94636fd432bd95f9d1ccbd1497ad7cc464bff3c6
laladaysz <leandralaura.dias@gmail.com>
2024-11-22 11:18:51 -0300

    :sparkles: feat: update task's status

--- tarefa/urls.py
+++ tarefa/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('tarefa/criar/', views.criar_tarefa, name='criar_tarefa'),
      path('tarefa/todas/', views.get_all_tarefas, name='get_all_tarefas'),
+     path('tarefa/alterar_status/<int:id>/', views.update_status, name='alterar_status'),       
 ]
--- tarefa/views.py
+++ tarefa/views.py
@@ -17,3 +17,23 @@ def get_all_tarefas(request):
     tarefas = Tarefa.objects.all()
     serializer = TarefaSerializer(tarefas, many=True)
     return Response(serializer.data)
+
+
+@api_view(['PATCH'])
+def update_status(request, id):
+    try:
+        tarefa = Tarefa.objects.get(id=id)
+    except Tarefa.DoesNotExist:
+        return Response({"error": "Tarefa não encontrada"}, status=status.HTTP_404_NOT_FOUND)
+
+    # Atualizando apenas o status da tarefa
+    novo_status = request.data.get("status")
+    if not novo_status:
+        return Response({"error": "Status é obrigatório"}, status=status.HTTP_400_BAD_REQUEST)
+
+    tarefa.status = novo_status
+    tarefa.save()
+
+    # Serializa a tarefa atualizada
+    serializer = TarefaSerializer(tarefa)
+    return Response(serializer.data, status=status.HTTP_200_OK)
